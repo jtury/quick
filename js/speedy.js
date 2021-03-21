018bfe1c2970d8f4cb17bffecbf78b0ffb548597
James Tury <49847213+jtury@users.noreply.github.com>
2021-03-21 00:51:45 -0400

    consolidated speedy.js; optimized js loading on index.html

--- js/speedy.js
+++ js/speedy.js
@@ -1,9 +1,8 @@
 let httpRequest;
-
 let cache = [];
 
 function fetch_information(url) {
-    let index = check_if_cached(url);
+    let index = cache.findIndex(element => element[0].includes(url));
     if (index === -1) {
         url = url + '/index.json';
         httpRequest = new XMLHttpRequest();
@@ -12,38 +11,32 @@ function fetch_information(url) {
             return false;
         }
 
-        httpRequest.onreadystatechange = parse_response;
+        httpRequest.onreadystatechange = function () {
+            if (httpRequest.readyState === XMLHttpRequest.DONE) {
+            if (httpRequest.status === 200) {
+                cache.push(
+                    [
+                        httpRequest.responseURL.replace("index.json", ""),
+                        JSON.parse(httpRequest.responseText)
+                    ]
+                );
+            } else {
+                alert("Hmm.. something's not right. Please email the developer and let them know you received an error of code #2. Thanks!");
+                return false;
+            }
+        }
+    };
         httpRequest.open('GET', url);
         httpRequest.send();
     }
     return true;
 }
 
-function parse_response() {
-    if (httpRequest.readyState === XMLHttpRequest.DONE) {
-        if (httpRequest.status === 200) {
-            cache.push(
-                [
-                    httpRequest.responseURL.replace("index.json", ""),
-                    JSON.parse(httpRequest.responseText)
-                ]
-            );
-        } else {
-            alert("Hmm.. something's not right. Please email the developer and let them know you received an error of code #2. Thanks!");
-            return false;
-        }
-    }
-}
-
-function check_if_cached(url) {
-    return cache.findIndex(element => element[0].includes(url))
-}
-
 function attach_hooks() { // this function enables prefetch links on all anchor tags with an href specified
     let anchor_array = Array.from(document.getElementsByTagName("a"));
     anchor_array.forEach(element => {
         if (element.href && new URL(element.href).origin === location.origin) {
-            element.addEventListener('pointerenter', function (event) {
+            element.addEventListener('pointerenter', function (/* event */) {
                 setTimeout(function () {
                     if (element.matches(':hover')) {
                         fetch_information(element.href)
@@ -60,5 +53,3 @@ function attach_hooks() { // this function enables prefetch links on all anchor 
         }
     });
 }
-
-attach_hooks();
