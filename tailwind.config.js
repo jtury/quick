1d24447d4251fba365d294e88b6b2e0c5055440b
James Tury <49847213+jtury@users.noreply.github.com>
2021-03-21 21:10:21 -0400

    added previously ignored files

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  purge: {
+    enabled: true,
+    content: [
+        'index.html',
+        'c/**/*.html'
+    ]
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
