d9663d2e44ed56c978747a37f3d6328da7408e46
James Tury <49847213+jtury@users.noreply.github.com>
2021-03-21 21:05:29 -0400

    First (working 100%) version

--- js/speedy.js
+++ js/speedy.js
@@ -1,6 +1,9 @@
 let httpRequest;
 let cache = [];
 
+
+//TODO: Fix bug with # links!
+
 function fetch_information(url) {
     let index = cache.findIndex(element => element[0].includes(url));
     if (index === -1) {
@@ -21,7 +24,7 @@ function fetch_information(url) {
                     ]
                 );
             } else {
-                alert("Hmm.. something's not right. Please email the developer and let them know you received an error of code #2. Thanks!");
+                alert("Something went wrong. Try disabling javascript and try again.");
                 return false;
             }
         }
@@ -41,15 +44,55 @@ function attach_hooks() { // this function enables prefetch links on all anchor 
                     if (element.matches(':hover')) {
                         fetch_information(element.href)
                     }
-                }, 100);
+                }, 65);
             });
-            element.addEventListener('pointerdown', function (event) {event.preventDefault()});
+            element.addEventListener('pointerdown', function (event) {
+                event.preventDefault();
+                fetch_information(element.href);
+            });
             element.addEventListener('click', function (event) {
                 event.preventDefault();
-                if (fetch_information(element.href) !== true) {
+                if (fetch_information(element.href) === true) {
+                    let url = element.href;
+                    display_page(cache.findIndex(element => element[0].includes(url)));
+                } else {
                     window.location = element.href;
                 }
             });
         }
     });
 }
+
+function init() {
+    cache.push([document.location.href, {
+        "title": document.title,
+        "page_content": document.getElementById("page").innerHTML
+    }]);
+
+    let url = cache[0][0];
+    let title = cache[0][1]["title"];
+    let html = cache[0][1]["page_content"];
+    window.history.replaceState({title, html},title, url);
+    attach_hooks();
+}
+
+function display_page(index) {
+    let url = cache[index][0];
+    let title = cache[index][1]["title"];
+    let html = cache[index][1]["page_content"];
+
+    document.title = title;
+    document.getElementById("page").innerHTML = html;
+    window.history.pushState({title, html},title, url);
+}
+
+window.onpopstate = function(event) {
+    let json = JSON.stringify(event.state);
+
+    if (json !== 'null') {
+        json = JSON.parse(json);
+        document.title = json["title"];
+        document.getElementById("page").innerHTML = json["html"];
+        attach_hooks();
+    }
+};
